b5c0797e577780b7dec8f9bb4b3d57bfd441eba9
Samuel Sylvester <sasy260902@gmail.com>
2024-05-15 11:22:09 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,8 @@
 import Image from "next/image";
 
-import Header from "./components/header/Header";
-import About from "./components/About";
-import Faqs from "./components/Faqs";
-import Footer from "./components/Footer";
+import MainPage from "./components/MainPage";
 export default function Home() {
   return (
-    <main className="w-full">
-      <Header />
-      <About />
-      <Faqs />
-      <Footer />
-    </main>
+    <Mainpage />
   );
 }
